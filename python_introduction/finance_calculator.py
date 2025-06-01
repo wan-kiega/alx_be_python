429c5659d54859fc914e8f1f2019416a275478a0
Joy Kiega <jwkiega@gmail.com>
2025-06-01 15:31:02 +0300

    new file added

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = 5000
+monthly_expenses = 4000
+monthly_savings = monthly_income - monthly_expenses
+print(monthly_savings)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(projected_savings)
